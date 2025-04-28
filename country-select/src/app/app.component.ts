88e79851fcae14caf359b7da092d1f2f59f13b0d
Henry Nguyen <nguyen.henry796@gmail.com>
2025-04-27 21:25:59 -0400

    Part D

--- country-select/src/app/app.component.ts
+++ country-select/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CountryService } from './country.service';
+import { MapComponent } from "./map/map.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
